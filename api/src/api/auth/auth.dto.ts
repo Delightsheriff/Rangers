b0971824abef6b3ce941a6122f489a89737cf3bc
Muliro1 <mulirokhaemba@gmail.com>
2025-03-29 21:54:24 +0300

    ft-auth-dto

---
+++ api/src/api/auth/auth.dto.ts
@@ -0,0 +1,10 @@
+export class SignUpDto {
+    readonly email: string;
+    readonly password: string;
+    readonly user_name: string;
+  }
+  
+  export class LoginDto {
+    readonly email: string;
+    readonly password: string;
+  }
