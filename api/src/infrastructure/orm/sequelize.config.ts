20525d4cf8425d06e235d7160907b94355ea76f9
Horleryheancarh <oladipoolayinka7@gmail.com>
2025-04-08 09:29:49 +0100

    Fix: update Sequelize configuration to enable SSL support

--- api/src/infrastructure/orm/sequelize.config.ts
+++ api/src/infrastructure/orm/sequelize.config.ts
@@ -4,14 +4,19 @@ import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from 'src/config';
 export const SequelizeConfig: Partial<SequelizeModuleOptions> = {
   dialect: 'postgres',
   host: DB_HOST,
-  port: DB_PORT,
+  port: DB_PORT ? Number(DB_PORT) : undefined,
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  ssl: true,
   autoLoadModels: true,
   synchronize: false,
   dialectOptions: {
     connectTimeout: 60 * 1000,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
   define: {
     timestamps: true,
