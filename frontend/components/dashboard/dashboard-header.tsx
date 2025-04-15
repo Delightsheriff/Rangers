b09864ff7b79fd119ab56db8ab57975e93c514a8
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-04-15 21:53:51 +0100

    feat: set up profile section

--- frontend/components/dashboard/dashboard-header.tsx
+++ frontend/components/dashboard/dashboard-header.tsx
@@ -100,7 +100,7 @@ export default function DashboardHeader() {
               <DropdownMenuSeparator />
               <DropdownMenuItem
                 className="cursor-pointer"
-                onClick={() => router.push("/profile")}
+                onClick={() => router.push("/dashboard/profile")}
               >
                 <User className="mr-2 h-4 w-4" />
                 Profile
