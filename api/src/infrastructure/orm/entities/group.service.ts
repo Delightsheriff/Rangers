68126b15f01fadb0a90407394a63e78ff6ff2d53
Muliro1 <mulirokhaemba@gmail.com>
2025-04-18 03:21:12 +0300

    ft-group-creation-route

--- api/src/infrastructure/orm/entities/group.service.ts
+++ api/src/infrastructure/orm/entities/group.service.ts
@@ -1,8 +1,14 @@
-// ...existing code...
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { GroupEntity } from './group.entity';
 import { UserEntity } from './user.entity';
 
 export class GroupService {
+    constructor(
+        @InjectModel(GroupEntity) private groupModel: typeof GroupEntity,
+        @InjectModel(UserEntity) private userModel: typeof UserEntity,
+      ) {}
+
   async createGroup(name: string, userIds: number[]) {
     const group = await GroupEntity.create({ name });
     const users = await UserEntity.findAll({ where: { id: userIds } });
--- api/src/infrastructure/orm/repositories/repository.module.ts
+++ api/src/infrastructure/orm/repositories/repository.module.ts
@@ -4,10 +4,13 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UserRepository } from './user.repository';
 import { GroupMemberEntity} from '../entities/group-member.entity';
 import { GroupEntity } from '../entities/group.entity';
+import { GroupService } from '../entities/group.service'
+import { GroupController } from '../entities/group.controller'
 
 @Module({
   imports: [SequelizeModule.forFeature([UserEntity, GroupEntity, GroupMemberEntity])],
-  providers: [UserRepository],
+  providers: [UserRepository, GroupService],
+  controllers: [ GroupController ],
   exports: [UserRepository],
 })
 export class RepositoryModule {}
