3736cab1e9bb147ef43dee7e8541251200c23184
Muliro1 <mulirokhaemba@gmail.com>
2025-04-18 03:44:02 +0300

    check-user-exists

--- api/src/infrastructure/orm/entities/group.service.ts
+++ api/src/infrastructure/orm/entities/group.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { GroupEntity } from './group.entity';
 import { UserEntity } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 export class GroupService {
     constructor(
@@ -9,10 +10,15 @@ export class GroupService {
         @InjectModel(UserEntity) private userModel: typeof UserEntity,
       ) {}
 
-  async createGroup(name: string, userIds: number[]) {
-    const group = await GroupEntity.create({ name });
-    const users = await UserEntity.findAll({ where: { id: userIds } });
-    await group.$set('members', users);
-    return group;
-  }
-}
+      async createGroup(name: string, userIds: number[]) {
+        const users = await this.userModel.findAll({ where: { id: userIds } });
+        const foundIds = users.map(u => u.id);
+        const notFound = userIds.filter(id => !foundIds.includes(id));
+        if (notFound.length > 0) {
+          throw new BadRequestException(`User(s) not found: ${notFound.join(', ')}`);
+        }
+        const group = await this.groupModel.create({ name });
+        await group.$set('members', users);
+        return group;
+      }
+    }
