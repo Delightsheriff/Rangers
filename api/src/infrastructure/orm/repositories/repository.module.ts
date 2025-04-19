daa5fc23a323545868c1e4c32ba2edaba32026c5
Muliro1 <mulirokhaemba@gmail.com>
2025-04-19 21:57:08 +0300

    ft-register-expense-entities

--- api/src/infrastructure/orm/repositories/repository.module.ts
+++ api/src/infrastructure/orm/repositories/repository.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserEntity } from '../entities/user.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserRepository } from './user.repository';
+import { Expense } from '../entities/expenses.entity';
+import { ExpenseParticipant } from '../entities/expenseParticipant.entity';
+import { ExpenseService } from '../entities/expenses.service';
+import { ExpenseController } from '../entities/expenses.controller'
 
 @Module({
-  imports: [SequelizeModule.forFeature([UserEntity])],
-  providers: [UserRepository],
+  imports: [SequelizeModule.forFeature([UserEntity, Expense, ExpenseParticipant])],
+  providers: [UserRepository, ExpenseService],
+  controllers: [ExpenseController],
   exports: [UserRepository],
 })
 export class RepositoryModule {}
